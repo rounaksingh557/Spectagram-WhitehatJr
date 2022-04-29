e8fd7a3bb7f03f901af7855047f13f0b648208eb
Rounak Singh <rounaksingh9431738764@gmail.com>
2022-04-29 08:02:13 +0530

    Completed the project and fixed dropdown picker (reduced version to 4.0.9)

--- Screens/CreatePost.js
+++ Screens/CreatePost.js
@@ -1,4 +1,3 @@
-// Modules Import
 import React, { Component } from "react";
 import {
   View,
@@ -8,29 +7,32 @@ import {
   Platform,
   StatusBar,
   Image,
+  ScrollView,
+  TextInput,
 } from "react-native";
+
 import { RFValue } from "react-native-responsive-fontsize";
-import { FlatList } from "react-native-gesture-handler";
+import DropDownPicker from "react-native-dropdown-picker";
 
-// Files Import
-import PostCard from "./PostCard";
-let posts = require("./temp_post.json");
-
-export default class Feed extends Component {
+export default class CreatePost extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      previewImage: "image_1",
+      dropdownHeight: 40,
+    };
   }
 
   componentDidMount() {}
 
-  renderItem = ({ item: post }) => {
-    return <PostCard post={post} />;
-  };
-
-  keyExtractor = (item, index) => index.toString();
-
   render() {
+    let preview_images = {
+      image_1: require("../assets/image_1.jpg"),
+      image_2: require("../assets/image_2.jpg"),
+      image_3: require("../assets/image_3.jpg"),
+      image_4: require("../assets/image_4.jpg"),
+      image_5: require("../assets/image_5.jpg"),
+    };
     return (
       <View style={styles.container}>
         <SafeAreaView style={styles.droidSafeArea} />
@@ -42,16 +44,64 @@ export default class Feed extends Component {
             ></Image>
           </View>
           <View style={styles.appTitleTextContainer}>
-            <Text style={styles.appTitleText}>Spectagram</Text>
+            <Text style={styles.appTitleText}>New Post</Text>
           </View>
         </View>
-        <View style={styles.cardContainer}>
-          <FlatList
-            keyExtractor={this.keyExtractor}
-            data={posts}
-            renderItem={this.renderItem}
-          />
+        <View style={styles.fieldsContainer}>
+          <ScrollView>
+            <Image
+              source={preview_images[this.state.previewImage]}
+              style={styles.previewImage}
+            ></Image>
+            <View style={{ height: RFValue(this.state.dropdownHeight) }}>
+              <DropDownPicker
+                items={[
+                  { label: "Image 1", value: "image_1" },
+                  { label: "Image 2", value: "image_2" },
+                  { label: "Image 3", value: "image_3" },
+                  { label: "Image 4", value: "image_4" },
+                  { label: "Image 5", value: "image_5" },
+                ]}
+                defaultValue={this.state.previewImage}
+                containerStyle={{
+                  height: 40,
+                  borderRadius: 20,
+                  marginBottom: 10,
+                }}
+                onOpen={() => {
+                  this.setState({ dropdownHeight: 170 });
+                }}
+                onClose={() => {
+                  this.setState({ dropdownHeight: 40 });
+                }}
+                style={{ backgroundColor: "transparent" }}
+                itemStyle={{
+                  justifyContent: "flex-start",
+                }}
+                dropDownStyle={{ backgroundColor: "#2a2a2a" }}
+                labelStyle={{
+                  color: "white",
+                }}
+                arrowStyle={{
+                  color: "white",
+                }}  
+                onChangeItem={(item) =>
+                  this.setState({
+                    previewImage: item.value,
+                  })
+                }
+              />
+            </View>
+
+            <TextInput
+              style={styles.inputFont}
+              onChangeText={(caption) => this.setState({ caption })}
+              placeholder={"Caption"}
+              placeholderTextColor="white"
+            />
+          </ScrollView>
         </View>
+        <View style={{ flex: 0.08 }} />
       </View>
     );
   }
@@ -71,7 +121,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   appIcon: {
-    flex: 0.2,
+    flex: 0.3,
     justifyContent: "center",
     alignItems: "center",
   },
@@ -81,14 +131,30 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
   },
   appTitleTextContainer: {
-    flex: 0.8,
+    flex: 0.7,
     justifyContent: "center",
   },
   appTitleText: {
     color: "white",
     fontSize: RFValue(28),
   },
-  cardContainer: {
+  fieldsContainer: {
     flex: 0.85,
+  },
+  previewImage: {
+    width: "93%",
+    height: RFValue(250),
+    alignSelf: "center",
+    borderRadius: RFValue(10),
+    marginVertical: RFValue(10),
+    resizeMode: "contain",
+  },
+  inputFont: {
+    height: RFValue(40),
+    borderColor: "white",
+    borderWidth: RFValue(1),
+    borderRadius: RFValue(10),
+    paddingLeft: RFValue(10),
+    color: "white",
   },
 });
